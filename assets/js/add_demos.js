ae32d6e93551942b554fd714481fab61d5b2933e
Wyatt H <jhawes@ucsc.edu>
2023-11-17 18:35:21 -0800

    Adding images to tech demos

--- assets/js/add_demos.js
+++ assets/js/add_demos.js
@@ -25,24 +25,24 @@ console.log("adding demos");
     {
       date: "2023",
       title: "Geocoins: Programming Pattern Practice",
-      imgSrc: "images/pic01.jpg",
+      imgSrc: "images/Demos/Geocoin.jpg",
       description:
         "This demo was mainly focused on creating code that is flexible and can be easily changed to meet changing requirements. It also served as practice for multiple types of design patterns while learning a new library. Those being the Flyweight Pattern, Singleton Pattern, and the Momento Pattern. The demo also uses the browsers local storage to save your progress between sessions. My biggest take away from this demo is that there are many ways to solve the same problem and one way is not always necessarily better than another. You must take in the context surrounding the problem and select a solution that works the best for you as well as the problem.",
       links: [
         {
           name: "Playable Link",
-          link: "https://",
+          link: "https://wyatt-hawes.github.io/cmpm-121-demo-3/",
         },
         {
           name: "Github Repository",
-          link: "https://",
+          link: "https://github.com/Wyatt-Hawes/cmpm-121-demo-3",
         },
       ],
     },
     {
       date: "2023",
       title: "Snake Buddies: Managing Constraints as a Team",
-      imgSrc: "images/pic01.jpg",
+      imgSrc: "images/Demos/Snake.png",
       description:
         "This demo is fairly similar to the Flytrap demo as they both use the `crisp-game` library which is intentionally quite restrictive. Despite this, my team and I were able to create a prototype to discover if it was possible to create an engaging multiplayer experience with only a single-input. My biggest takeaway is that communication is key for a team that is heavily constrained as everyone should be in sync with their ideas. I also discovered constraints heavy constraints force successful collaboration and teamwork.",
       links: [
@@ -59,7 +59,7 @@ console.log("adding demos");
     {
       date: "2023",
       title: "Flytrap: Managing Constraints",
-      imgSrc: "images/pic01.jpg",
+      imgSrc: "images/Demos/Flytrap.jpg",
       description:
         "A demo made in the `crisp-game` library which is extremely limited in terms of capability as it only allows for a single user input in the form of a button press. This demo served as a way to learn a new library but also how to work around constraints. My biggest takeaway from this demo is that constraints can often lead to creative and clever solutions that push the boundaries of what is possible.",
       links: [
@@ -76,7 +76,7 @@ console.log("adding demos");
     {
       date: "2023",
       title: "Shade: Command Pattern Practice",
-      imgSrc: "images/pic01.jpg",
+      imgSrc: "images/Demos/Shade.JPG",
       description:
         "A demo focused on practicing the Command Design Pattern to create a prototype. The character movements are abstracted to commands which allows their actions to be replayed by 'shades' that can still influence the game world in the same way the player could. The main takeaway that I had from this demo is just how much easier following `good coding practices` can speed up development time.",
       links: [
@@ -93,7 +93,7 @@ console.log("adding demos");
     {
       date: "2023",
       title: "Paint: Programming Pattern Demo",
-      imgSrc: "images/pic01.jpg",
+      imgSrc: "images/Demos/Sketchpad.JPG",
       description:
         "In addition to continuing the practice of incremental development, this demo serves as an application of the 'Command Design Pattern' which separates a command call from the command itself allowing for ease in implementing undo and redo capabilities. By abstracting commands in such a manner, it allows them to be easily tested and applied to different targets which is used to easily implement an high definition export of your drawing. There is also still a focus on data-driven design by allowing users to add as many custom stickers as they want.",
       links: [
@@ -110,7 +110,7 @@ console.log("adding demos");
     {
       date: "2023",
       title: "Clicker Game: Incremental Development",
-      imgSrc: "images/pic01.jpg",
+      imgSrc: "images/Demos/Clicker.JPG",
       description:
         "A practice of creating a product through incremental changes and development, ensuring that you are delivering a useable product at every step along the way. A main focus was also good coding practices, ensuring that you are coding in a scalable and efficient manner. Creating upgrades in a data-driven manner allows designers to not have to understand the inner workings of the program, rather just how they interface with it.",
       links: [
@@ -127,7 +127,7 @@ console.log("adding demos");
     {
       date: "2022",
       title: "iSpy: Phaser Demo",
-      imgSrc: "images/pic01.jpg",
+      imgSrc: "images/Demos/iSpy.JPG",
       description:
         "A demo created to put together all the skills from previous demo's to create a more complete experience. Combining animation, scenes, and interaction to create a playable experience. The main takeaway from this demo was that it is extremely important to test interactions as there are many edge-cases that can cause the entire experience to break.",
       links: [
@@ -144,7 +144,7 @@ console.log("adding demos");
     {
       date: "2022",
       title: "Cinematic: Phaser Scene Demo",
-      imgSrc: "images/pic01.jpg",
+      imgSrc: "images/Demos/Animation.JPG",
       description:
         "A demo made to practice creating simple assets and 'tween-based' animation. Tweens are a large part of Phaser and many other engines so gaining the knowledge on how they work as well as their use cases is extremely helpful. I learned that it is often very useful to spend the time looking for tools that can do the task that you are looking for instead of focusing on creating it all by yourself.",
       links: [
@@ -161,7 +161,7 @@ console.log("adding demos");
     {
       date: "2022",
       title: "Plink: Phaser Physics Demo",
-      imgSrc: "images/pic01.jpg",
+      imgSrc: "images/Demos/Plinko.JPG",
       description:
         "A demo made to practice the use of scenes in the Phaser game engine as well as learn how to use and apply a physics engine. I mainly learned that being able to work with the restrictions placed upon you by a specific engine or library can be both a positive and negative experience. On one hand, it may be overly challenging to complete your idea, but restrictions lead to additional creativity which more often than not, outweighs the pain of figuring out a new library.",
       links: [
