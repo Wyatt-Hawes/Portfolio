4d57d35a1a2b95aa5115c26f101f449eacad0699
Wyatt H <jhawes@ucsc.edu>
2024-01-28 16:51:34 -0800

    Adding Winter 2023 courses

--- assets/js/add_coursework.js
+++ assets/js/add_coursework.js
@@ -1,131 +1,149 @@
 //Add classes to the completed_coursework.html in a more data-driven way (cause im tired of copy and pasting)
-console.log("adding projects");
+console.log('adding projects');
 {
   const courses = [
     {
-      date: "Fall 2023",
-      title: "Development Patterns (CMPM-121)",
+      date: 'Winter 2024',
+      title: 'Full Stack Web Development I (CSE-186)',
       description:
-        "Advanced programming focused on software design patterns and refactoring. Introduces classic software design patterns, as well as game programming patterns. Introduces software refactoring, including code smells and widely used refactoring patterns.",
+        'This course examines current uses of the World-Wide Web for delivery of the sophisticated interactive applications we use daily. Web Applications offer several advantages over traditional, locally installable binaries including heterogeneous deployment, instantaneous access, continuous updates, and the possibility of collaboration at scale. These Full Stack Web Applications are typically more complex than their traditional counterparts, requiring the seamless integrating of numerous related technologies if end users are to have a productive experience and the system is to remain robust, performant, and secure.',
     },
     {
-      date: "Fall 2023",
-      title: "Game Design Studio I - Rapid Prototyping (CMPM-170)",
+      date: 'Winter 2024',
+      title: 'Advanced Programming II (CMPM-123)',
       description:
-        "Students in CMPM 170 “Rapid Prototyping” will learn how to evaluate their visions for larger projects by first creating smaller prototypes. Teams will create different prototypes in three development “sprints” with the aim of quickly arriving at, executing, and testing the validity of project ideas. This class is meant to emphasize process over product and experimentation over polish. Therefore, skills core to process and professionalization will also be addressed. Particular emphasis will be placed on the importance of core interpersonal skills in teamwork and production. Practicing these core skills will better prepare students to transition into professional team settings.",
+        'Course introduces students to current and emerging advanced programming methods used in building complex, high-performance, and networked interactive media systems. Students use tools like debuggers and profiles to inspect and resolve software performance bottlenecks in a compiled language.',
     },
     {
-      date: "Fall 2023",
-      title: "Game Systems (CMPM-176)",
+      date: 'Winter 2024',
+      title: 'Game Design Studio II - Vertical Slice (CMPM - 171)',
       description:
-        "Presents game design as the interplay of multiple interacting game systems. Surveys various game systems: movement, combat, reward, economic, logistics, quest, information visibility, narrative. Students explore systems via study, design, and play of board, card, and computer games.",
+        'Students work in teams to produce fully functional games based on previously developed game subsets. Focus on production including scaling, long-term player experience, teaching mechanics through interaction, developing multi-stage narratives/scenarios, and varying gameplay as players develop new understandings and abilities.',
     },
     {
-      date: "Summer 2023",
-      title: "AI (CMPM-146)",
+      date: 'Fall 2023',
+      title: 'Development Patterns (CMPM-121)',
       description:
-        "This class examines the use of Artificial Intelligence (AI) in games. It covers core AI technologies for searching through graph search algorithms, behavior control through state machines and behavior trees, and learning through neural networks and evolutionary learning. Application is relating to the use of AI to improve game design, development, and game play. It examines the AI content in multiple commercial games.",
+        'Advanced programming focused on software design patterns and refactoring. Introduces classic software design patterns, as well as game programming patterns. Introduces software refactoring, including code smells and widely used refactoring patterns.',
     },
     {
-      date: "Spring 2023",
-      title: "Advanced Programming (CSE-111)",
+      date: 'Fall 2023',
+      title: 'Game Design Studio I - Rapid Prototyping (CMPM-170)',
       description:
-        "An introduction to object-oriented techniques of software development including data abstraction, inheritance, polymorphism, and object-oriented design. Extensive practice using a computer to solve problems, including construction of graphical user interfaces and a multithreaded client/server applications.",
+        'Students in CMPM 170 “Rapid Prototyping” will learn how to evaluate their visions for larger projects by first creating smaller prototypes. Teams will create different prototypes in three development “sprints” with the aim of quickly arriving at, executing, and testing the validity of project ideas. This class is meant to emphasize process over product and experimentation over polish. Therefore, skills core to process and professionalization will also be addressed. Particular emphasis will be placed on the importance of core interpersonal skills in teamwork and production. Practicing these core skills will better prepare students to transition into professional team settings.',
     },
     {
-      date: "Spring 2023",
-      title: "Game Development Experience  (CMPM-120)",
+      date: 'Fall 2023',
+      title: 'Game Systems (CMPM-176)',
       description:
-        "Teaches the concrete programming and collaboration skills associated with making a digital game from start to finish, including but not limited to: establishing a team, concepting, storyboarding, prototyping, producing, and testing a game for release. Students are organized into groups and work together to create and produce a playable game.",
+        'Presents game design as the interplay of multiple interacting game systems. Surveys various game systems: movement, combat, reward, economic, logistics, quest, information visibility, narrative. Students explore systems via study, design, and play of board, card, and computer games.',
     },
     {
-      date: "Spring 2023",
-      title: "Game Design Experience (ARTG-120)",
+      date: 'Summer 2023',
+      title: 'AI (CMPM-146)',
       description:
-        "Teaches the concrete skills associated with making a digital game, from start to finish. Activities include establishing a team, concepting, storyboarding, prototyping, producing, and testing a game for release. Students are organized into groups and work together to create and produce a playable game.",
+        'This class examines the use of Artificial Intelligence (AI) in games. It covers core AI technologies for searching through graph search algorithms, behavior control through state machines and behavior trees, and learning through neural networks and evolutionary learning. Application is relating to the use of AI to improve game design, development, and game play. It examines the AI content in multiple commercial games.',
     },
     {
-      date: "Winter 2023",
-      title: "Introduction to Data Structures and Algorithms  (CSE-101)",
+      date: 'Spring 2023',
+      title: 'Advanced Programming (CSE-111)',
       description:
-        "Introduction to abstract data types and basics of algorithms. Linked lists, stacks, queues, hash tables, trees, heaps, and graphs will be covered. Students will also be taught how to derive big-Oh analysis of simple algorithms. All assignments will be in C/C++.",
+        'An introduction to object-oriented techniques of software development including data abstraction, inheritance, polymorphism, and object-oriented design. Extensive practice using a computer to solve problems, including construction of graphical user interfaces and a multithreaded client/server applications.',
     },
     {
-      date: "Winter 2023",
-      title: "Foundations of Video Game Design (CMPM-80k)",
+      date: 'Spring 2023',
+      title: 'Game Development Experience  (CMPM-120)',
       description:
-        "A generally accessible course in which students explore how video games (and games generally) shape experiences and express ideas. Students develop novel games, engage in game interpretation, and survey related topics (e.g., game history, technology, narrative, and ethics)",
+        'Teaches the concrete programming and collaboration skills associated with making a digital game from start to finish, including but not limited to: establishing a team, concepting, storyboarding, prototyping, producing, and testing a game for release. Students are organized into groups and work together to create and produce a playable game.',
     },
     {
-      date: "Winter 2023",
-      title: "Video Games as Visual Culture (FILM-80V)",
+      date: 'Spring 2023',
+      title: 'Game Design Experience (ARTG-120)',
       description:
-        "Through aesthetic, medium-specific and critical theories of electronic games, course introduces histories, ideas, and debates that inform critical game studies. Themes include: games and cinema; race, class, gender, sexuality and representation; visual/cultural studies approaches; topical issues in games.",
+        'Teaches the concrete skills associated with making a digital game, from start to finish. Activities include establishing a team, concepting, storyboarding, prototyping, producing, and testing a game for release. Students are organized into groups and work together to create and produce a playable game.',
     },
     {
-      date: "Fall 2022",
-      title: "Computer Systems and C Programming (CSE-13S)",
+      date: 'Winter 2023',
+      title: 'Introduction to Data Structures and Algorithms  (CSE-101)',
       description:
-        "C programming, command line, shell programming, editors, debuggers, source code control, and other tools. Basic computer systems, algorithm design and development, data types, program structures. Develops understanding of process model, compile-link-execute build cycle, language-machine interface, memory, and data representation.",
+        'Introduction to abstract data types and basics of algorithms. Linked lists, stacks, queues, hash tables, trees, heaps, and graphs will be covered. Students will also be taught how to derive big-Oh analysis of simple algorithms. All assignments will be in C/C++.',
     },
     {
-      date: "Fall 2022",
-      title: "Multivariate Calculus for Engineers (AM-30)",
+      date: 'Winter 2023',
+      title: 'Foundations of Video Game Design (CMPM-80k)',
       description:
-        "Advanced multivariate calculus for engineering majors. Coordinate systems, parametric curves, and surfaces; partial derivatives, gradient, Taylor expansion, stationary points, constrained optimization; integrals in multiple dimensions; integrals over curves and surfaces. Applications to engineering form an integral part of the course.",
+        'A generally accessible course in which students explore how video games (and games generally) shape experiences and express ideas. Students develop novel games, engage in game interpretation, and survey related topics (e.g., game history, technology, narrative, and ethics)',
     },
     {
-      date: "Spring 2022",
-      title: "Computer Systems and Assembly Language (CSE-12)",
+      date: 'Winter 2023',
+      title: 'Video Games as Visual Culture (FILM-80V)',
       description:
-        "Introduction to computer systems and assembly language and how computers compute in hardware and software. Topics include digital logic, number systems, data structures, compiling/assembly process, basics of the system software, and computer architecture.",
+        'Through aesthetic, medium-specific and critical theories of electronic games, course introduces histories, ideas, and debates that inform critical game studies. Themes include: games and cinema; race, class, gender, sexuality and representation; visual/cultural studies approaches; topical issues in games.',
     },
     {
-      date: "Spring 2022",
-      title: "Mathematical Methods For Engineers (AM-10)",
+      date: 'Fall 2022',
+      title: 'Computer Systems and C Programming (CSE-13S)',
       description:
-        "Applications-oriented course on complex numbers and linear algebra integrating Matlab as a computational support tool. Introduction to complex algebra. Vectors, bases and transformations, matrix algebra, solutions of linear systems, inverses and determinants, eigenvalues and eigenvectors, and geometric transformations.",
+        'C programming, command line, shell programming, editors, debuggers, source code control, and other tools. Basic computer systems, algorithm design and development, data types, program structures. Develops understanding of process model, compile-link-execute build cycle, language-machine interface, memory, and data representation.',
     },
     {
-      date: "Winter 2022",
-      title: "Applied Discrete Mathematics (CSE-16)",
+      date: 'Fall 2022',
+      title: 'Multivariate Calculus for Engineers (AM-30)',
       description:
-        "Introduction to applications of discrete mathematical systems. Topics include sets, functions, relations, graphs, predicate calculus, mathematical proof methods (induction, contraposition, contradiction), counting methods (permutations, combinations), and recurrences. Examples are drawn from computer science and computer engineering.",
+        'Advanced multivariate calculus for engineering majors. Coordinate systems, parametric curves, and surfaces; partial derivatives, gradient, Taylor expansion, stationary points, constrained optimization; integrals in multiple dimensions; integrals over curves and surfaces. Applications to engineering form an integral part of the course.',
     },
     {
-      date: "Fall 2021",
-      title: "Programming Abstractions in Python (CSE-30)",
+      date: 'Spring 2022',
+      title: 'Computer Systems and Assembly Language (CSE-12)',
       description:
-        "Introduction to software development in Python focusing on structuring software in terms of objects endowed with primitive operations. Introduces concepts and techniques via a sequence of concrete case studies.",
+        'Introduction to computer systems and assembly language and how computers compute in hardware and software. Topics include digital logic, number systems, data structures, compiling/assembly process, basics of the system software, and computer architecture.',
     },
     {
-      date: "Fall 2021",
-      title: "Visual Communication and Interaction Design <br> (ARTG-80G)",
+      date: 'Spring 2022',
+      title: 'Mathematical Methods For Engineers (AM-10)',
       description:
-        "Survey of the basics of visual communication and interaction design, focusing on communicating designs of interactive systems. Covers techniques from a breadth of visual communication traditions; how to choose, use, and innovate; and how to structure dialogue around them.",
+        'Applications-oriented course on complex numbers and linear algebra integrating Matlab as a computational support tool. Introduction to complex algebra. Vectors, bases and transformations, matrix algebra, solutions of linear systems, inverses and determinants, eigenvalues and eigenvectors, and geometric transformations.',
     },
     {
-      date: "Tested Out",
-      title: "Intro to Python (CSE-20)",
+      date: 'Winter 2022',
+      title: 'Applied Discrete Mathematics (CSE-16)',
       description:
-        "Provides students with Python programming skills and the ability to design programs and read Python code. Topics include data types, control flow, methods and advanced functions, built-in data structures, and introduction to OOP. No prior programming experience is required.",
+        'Introduction to applications of discrete mathematical systems. Topics include sets, functions, relations, graphs, predicate calculus, mathematical proof methods (induction, contraposition, contradiction), counting methods (permutations, combinations), and recurrences. Examples are drawn from computer science and computer engineering.',
     },
     {
-      date: "Tested Out",
-      title: "Honors Calculus B (MATH-20B)",
+      date: 'Fall 2021',
+      title: 'Programming Abstractions in Python (CSE-30)',
+      description:
+        'Introduction to software development in Python focusing on structuring software in terms of objects endowed with primitive operations. Introduces concepts and techniques via a sequence of concrete case studies.',
+    },
+    {
+      date: 'Fall 2021',
+      title: 'Visual Communication and Interaction Design <br> (ARTG-80G)',
+      description:
+        'Survey of the basics of visual communication and interaction design, focusing on communicating designs of interactive systems. Covers techniques from a breadth of visual communication traditions; how to choose, use, and innovate; and how to structure dialogue around them.',
+    },
+    {
+      date: 'Tested Out',
+      title: 'Intro to Python (CSE-20)',
+      description:
+        'Provides students with Python programming skills and the ability to design programs and read Python code. Topics include data types, control flow, methods and advanced functions, built-in data structures, and introduction to OOP. No prior programming experience is required.',
+    },
+    {
+      date: 'Tested Out',
+      title: 'Honors Calculus B (MATH-20B)',
       description:
         "Orbital mechanics, techniques of integration, and separable differential equations. Taylor expansions and error estimates, the Gaussian integral, Gamma function and Stirling's formula. Series and power series, numerous applications to physics.",
     },
     {
-      date: "Tested Out",
-      title: "Honors Calculus A (MATH-20A)",
+      date: 'Tested Out',
+      title: 'Honors Calculus A (MATH-20A)',
       description:
-        "Methods of proof, number systems, binomial and geometric sums. Sequences, limits, continuity, and the definite integral. The derivatives of the elementary functions, the fundamental theorem of calculus, and the main theorems of differential calculus.",
+        'Methods of proof, number systems, binomial and geometric sums. Sequences, limits, continuity, and the definite integral. The derivatives of the elementary functions, the fundamental theorem of calculus, and the main theorems of differential calculus.',
     },
   ];
 
   //Grab project posting container
-  const projectContainer = document.getElementById("courseworkPostSection");
+  const projectContainer = document.getElementById('courseworkPostSection');
 
   courses.forEach((course) => {
     createProjectElement(course);
@@ -143,14 +161,14 @@ console.log("adding projects");
 	</article>
     */
     //
-    const article = document.createElement("article");
-    const header = document.createElement("header");
-    const span_date = document.createElement("span");
-    const h2 = document.createElement("h2");
-    const description = document.createElement("p");
+    const article = document.createElement('article');
+    const header = document.createElement('header');
+    const span_date = document.createElement('span');
+    const h2 = document.createElement('h2');
+    const description = document.createElement('p');
 
     //Set correct class names
-    span_date.className = "date";
+    span_date.className = 'date';
 
     //Set date
     span_date.textContent = course.date;
